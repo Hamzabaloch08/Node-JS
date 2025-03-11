9d78f4955777c1d56eb1ef90a67d869f614f68eb
Hamzabaloch08 <hb.developer08@gmail.com>
2025-03-11 16:15:21 +0500

    learn modules in node js or start creating server

---
+++ Modules-in-NodeJS/app.js
@@ -0,0 +1,8 @@
+const {add, sub} = require('./math')
+
+console.log('Add Value is', add(10,5))
+console.log('Sub Value is', sub(10,5))
+
+// console.log('helllooo there..!')
+
+// console.log(math(10,10))
