30a95770f4ba90f8c125a36dd13772857bdc0696
Hamzabaloch08 <hb.developer08@gmail.com>
2025-03-13 02:09:14 +0500

    learn what is server in the first place

--- Node-JS-at-smit/express.JS/app.js
+++ Node-JS-at-smit/express.JS/app.js
@@ -6,6 +6,9 @@ const port = 200
 app.get('/',(req,res)=>{
     res.send('Hello world!')
 })
+app.get('/login',(req,res)=>{
+    res.send('Hi Login your account!')
+})
 
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}...!`)
