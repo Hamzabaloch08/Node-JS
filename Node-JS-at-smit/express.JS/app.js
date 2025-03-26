0c9312cd201358b437168e21b833b4808499c3a6
Hamzabaloch08 <hb.developer08@gmail.com>
2025-03-26 21:22:44 +0500

    Update app.js

--- Node-JS-at-smit/express.JS/app.js
+++ Node-JS-at-smit/express.JS/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 const app = express();
+const port = 3000
 
 app.get('/', (req, res) => {
     res.send(`Hello world! ${new Date()}`)
@@ -10,7 +11,7 @@ app.get('/login', (req, res) => {
     res.send('Hi Login your account!')
 })
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`Example server listening on port ${PORT}...!`)
+// const PORT = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Example server listening on port ${port}...!`)
 })
