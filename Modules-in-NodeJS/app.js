7353f51fe21ab53f8dd21c1b1b46aeb8c416e5fc
Hamzabaloch08 <hb.developer08@gmail.com>
2025-07-31 03:11:29 +0500

    delete old files

--- Modules-in-NodeJS/app.js
+++
@@ -1,8 +0,0 @@
-const {add, sub} = require('./math')
-
-console.log('Add Value is', add(10,5))
-console.log('Sub Value is', sub(10,5))
-
-// console.log('helllooo there..!')
-
-// console.log(math(10,10))
