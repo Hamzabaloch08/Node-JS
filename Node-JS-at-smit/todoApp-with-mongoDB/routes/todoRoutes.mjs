8a68eb1593fc79821bfac6af4fcd964c3b137e0e
Hamzabaloch08 <hb.developer08@gmail.com>
2025-08-05 05:37:44 +0500

    Update todoRoutes.mjs

--- Node-JS-at-smit/todoApp-with-mongoDB/routes/todoRoutes.mjs
+++ Node-JS-at-smit/todoApp-with-mongoDB/routes/todoRoutes.mjs
@@ -1,20 +1,18 @@
 import express from "express";
 import {
+  createTodo,
   getTodos,
   getSingleTodo,
-  createTodo,
   updateTodo,
   deleteTodo,
   deleteAllTodos,
-} from "../controllers/todoController.js";
+} from "../controllers/todoController.mjs";
 
-const router = express.Router();
+export const todoRoutes = express.Router();
 
-router.get("/todos", getTodos);
-router.get("/todos/:id", getSingleTodo)
-router.post("/todos", createTodo);
-router.put("/todos/:id", updateTodo);
-router.delete("/todos/:id", deleteTodo);
-router.delete("/todos", deleteAllTodos);
-
-export default router;
+todoRoutes.get("/todos", getTodos);
+todoRoutes.get("/todos/:id", getSingleTodo);
+todoRoutes.post("/todos", createTodo);
+todoRoutes.put("/todos/:id", updateTodo);
+todoRoutes.delete("/todos/:id", deleteTodo);
+todoRoutes.delete("/todos", deleteAllTodos);
